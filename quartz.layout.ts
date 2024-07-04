9ec530f7f976b52dbf9d728ba5f1dc0a87d63172
Player <98722040+Player1041@users.noreply.github.com>
2024-07-04 17:13:07 +0100

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,12 +25,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
+    Component.MobileOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.TableOfContents())
   ],
 }
 
