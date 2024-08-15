3eedba154e047e9fd570cbec220495d8086f7cc9
Player <redacteddoesit@gmail.com>
2024-08-16 00:15:32 +0100

    hide college folder

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,7 +25,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(Component.Explorer({
+      title: "Explorer",
+      folderDefaultState: "collapsed",
+      filterFn: (node) => node.name !== "college"
+    })),
   ],
   right: [
     Component.Graph(),
